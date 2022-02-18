4bccdd816fa23c32d83c878df047e53fc0850086
jmartino98 <joe427martino@outlook.com>
2022-02-17 20:45:07 -0500

    edit

---
+++ w6javascript/calc.js
@@ -0,0 +1,27 @@
+var myButton = document.querySelector('button')
+
+myButton.addEventListener('click', dataPrompt)
+
+
+function dataPrompt() {
+
+    var hourlyPayNumber = prompt('Enter your hourly pay', 'Hourly Pay')
+    var hoursWorkedNumber = prompt('Enter the amount of hours youve worked this week', 'Hours Worked This Week')
+    var taxRateNumber = prompt('Enter the tax rate', 'Tax Rate')
+    
+    var grossPayResult = Number(hourlyPayNumber) * Number(hoursWorkedNumber)
+    var uncleSamsShare = Number(grossPayResult) * Number(taxRateNumber)
+    var netPayResult = Number(grossPayResult) - Number(uncleSamsShare)
+    
+    
+    document.querySelector('span').innerHTML = `
+    Gross Pay: $${grossPayResult}
+    Uncle Sams Share: $${uncleSamsShare}
+    Net Pay: $${netPayResult}`
+
+    console.log(`
+    Gross Pay: $${grossPayResult}
+    Uncle Sams Share: $${uncleSamsShare}
+    Net Pay: $${netPayResult}`)
+
+}
